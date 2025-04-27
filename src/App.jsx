afdfb9f59569be2c51f0dff2aba12328afc06968
Carlos Eduardo Maximino <cmax15@outlook.com.br>
2025-04-27 18:26:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import './App.css';
 import { createClient } from '@supabase/supabase-js';
 
 // Configuração do Supabase
-const supabase = createClient('https://your-project-id.supabase.co', 'your-anon-key');
-
+const supabase = createClient('https://ongdxywgxszpxopxqfyq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZ2R4eXdneHN6cHhvcHhxZnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NzQ5OTYsImV4cCI6MjA2MTM1MDk5Nn0.Z3utIhlvB4lbb3GghbwDiLno8EEmLqcthVhxiguI70c');
 function App() {
   const [file1, setFile1] = useState(null);
   const [file2, setFile2] = useState(null);
