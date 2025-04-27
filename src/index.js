a840ac4d473bfea7312b0a2a6071e11050e673ab
Carlos Eduardo Maximino <cmax15@outlook.com.br>
2025-04-27 07:09:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import App from "./src/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-export default function Home() {
-  return <App />;
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
