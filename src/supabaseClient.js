c52ec531a1891908e9b16847819c74d4e87830cc
Carlos Eduardo Maximino <cmax15@outlook.com.br>
2025-04-27 14:39:16 -0300

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ongdxywgxszpxopxqfyq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZ2R4eXdneHN6cHhvcHhxZnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NzQ5OTYsImV4cCI6MjA2MTM1MDk5Nn0.Z3utIhlvB4lbb3GghbwDiLno8EEmLqcthVhxiguI70c
+';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
