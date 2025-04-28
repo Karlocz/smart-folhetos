d35f13a010d10f57817d7137b2ec8b7c0b309980
Carlos Eduardo Maximino <cmax15@outlook.com.br>
2025-04-27 21:02:33 -0300

    Update supabaseClient.js

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,11 @@
+// src/services/supabaseClient.js
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ongdxywgxszpxopxqfyq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZ2R4eXdneHN6cHhvcHhxZnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NzQ5OTYsImV4cCI6MjA2MTM1MDk5Nn0.Z3utIhlvB4lbb3GghbwDiLno8EEmLqcthVhxiguI70c';
+// Substitua o URL e a chave abaixo pelos valores do seu projeto Supabase
+const supabaseUrl = 'https://ongdxywgxszpxopxqfyq.supabase.co'; // Endereço do seu Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZ2R4eXdneHN6cHhvcHhxZnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NzQ5OTYsImV4cCI6MjA2MTM1MDk5Nn0.Z3utIhlvB4lbb3GghbwDiLno8EEmLqcthVhxiguI70c'; // Sua chave de API
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
